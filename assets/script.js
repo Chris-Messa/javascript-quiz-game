ca2622ef4757075e146d33a52692126c2c081ef4
Chris Messa <blindsun95@gmail.com>
2023-03-25 20:05:56 -0500

    restructuring functions

--- assets/script.js
+++ assets/script.js
@@ -8,74 +8,78 @@ const answerResult = document.querySelector("#answer-result");
 startButton.addEventListener("click", e => {
     e.stopPropagation();
     e.preventDefault();
-    document.querySelector("h1").setAttribute("style", "display: none;")
-    document.querySelector("#start-button").setAttribute("style", "display: none;")
-    displayQuestion();
-    displayAnswers();
+    document.querySelector("h1").setAttribute("style", "display: none;");
+    document.querySelector("#start-button").setAttribute("style", "display: none;");
+    chooseQuestion();
 })
 
 const questions = [
     {
         question: "question1", 
         answer: "choice 1", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "1 choice 1", "1 choice 2", "1 choice 3", "1 choice 4"]
     }, 
     {
         question: "question2", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "2 choice 1", "2 choice 2", "2 choice 3", "2 choice 4"]
     }, 
     {
         question: "question3", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "3 choice 1", "3 choice 2", "3 choice 3", "3 choice 4"]
     }, 
     {
         question: "question4", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "4 choice 1", "4 choice 2", "4 choice 3", "4 choice 4"]
     }, 
     {
         question: "question5", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "5 choice 1", "5 choice 2", "5 choice 3", "5 choice 4"]
     }, 
     {
         question: "question6", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "6 choice 1", "6 choice 2", "6 choice 3", "6 choice 4"]
     }, 
     {
         question: "question7", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "7 choice 1", "7 choice 2", "7 choice 3", "7 choice 4"]
     }, 
     {
         question: "question8", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "8 choice 1", "8 choice 2", "8 choice 3", "8 choice 4"]
     }, 
     {
-        question: "question1", 
+        question: "question9", 
         answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
+        choices: [ "9 choice 1", "9 choice 2", "9 choice 3", "9 choice 4"]
     }, 
     {
-        question: "question1", 
+        question: "question10", 
         answer: "HO", 
         choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
-    }, 
-    {
-        question: "question1", 
-        answer: "HO", 
-        choices: [ "choice 1", "choice 2", "choice 3", "choice 4"]
-    }, 
+    }
 ];
 
-let randomQ = questions[Math.floor(Math.random() * questions.length)].question;
-function displayQuestion() {
-    quizQuestions.textContent = randomQ;
+
+
+
+function chooseQuestion() {
+    createListEls();
+    let randomQ = questions[Math.floor(Math.random() * questions.length)];
+    questionText = randomQ.question;
+    quizQuestions.textContent = questionText;
+    console.log(randomQ.choices);
+    for (let i = 0; i < 4; i++) {
+        answerList.children[i].textContent = randomQ.choices[i];
+    }
 }
+
 
 function createListEls() {
     for (let i = 0; i < 4; i++) {
@@ -84,14 +88,6 @@ function createListEls() {
     }
 }
 
-
-
-function displayAnswers() {
-    let choices = ["choice 1", "choice 2", "choice 3", "choice 4"];
-    for (let i = 0; i < 4; i++) {
-        answerList.children[i].textContent = choices[i];
-    }
-}
 
 
 
