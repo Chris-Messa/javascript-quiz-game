ec297cd0d6df93497e010653d105ed144c8898ef
Chris Messa <blindsun95@gmail.com>
2023-03-28 21:36:36 -0500

    timer function

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,7 @@ const startButton = document.querySelector(`#start-button`);
 const quizQuestions = document.querySelector(`#quiz-questions`);
 const answerList = document.querySelector(`#answer-list`);
 const answerResult = document.querySelector(`#answer-result`);
-
-
-startButton.addEventListener(`click`, e => {
-    e.stopPropagation();
-    e.preventDefault(); 
-    playGame();
-})
-
+const h1El =  document.querySelector(`h1`);
 const questions = [
     {
         question: `Which of the following is not an array method?`, 
@@ -41,87 +34,64 @@ const questions = [
         question: `In the following array: [1, 2, 3, 4, 5, 6, 7] what is the value at the 3rd index?`, 
         answer: `4`, 
         choices: [ `3`, `4`, `2`, `There is no value at the 3rd index`]
-    }, 
-    {
-        question: ``, 
-        answer: `HO`, 
-        choices: [ `7 choice 1`, `7 choice 2`, `7 choice 3`, `7 choice 4`]
-    }, 
-    {
-        question: `question8`, 
-        answer: `HO`, 
-        choices: [ `8 choice 1`, `8 choice 2`, `8 choice 3`, `8 choice 4`]
-    }, 
-    {
-        question: `question9`, 
-        answer: `HO`, 
-        choices: [ `9 choice 1`, `9 choice 2`, `9 choice 3`, `9 choice 4`]
-    }, 
-    {
-        question: `question10`, 
-        answer: `HO`, 
-        choices: [ `choice 1`, `choice 2`, `choice 3`, `choice 4`]
     }
 ];
 
-const questionOne = questions[0];
-let chosenAnswer ="";
+answerList.setAttribute(`style`, `display: none`)
+var timerCountdown = 3;
+startButton.addEventListener(`click`, e => {
+    e.stopPropagation();
+    e.preventDefault(); 
+    playGame();
+    timer.textContent = timerCountdown;
+
+    const id = setInterval(() => {
+        timerCountdown--; 
+        timer.textContent = timerCountdown;
+        if (timerCountdown === 0) {
+            clearInterval(id);
+            console.log("Times UP");
+        }
+    }, 1000)
+ 
+});
 
 function playGame() {
-    document.querySelector(`h1`).setAttribute(`style`, `display: none;`);
-    document.querySelector(`#start-button`).setAttribute(`style`, `display: none;`);
-    chooseQuestion();
+    h1El.setAttribute(`style`, `display: none;`);
+    startButton.setAttribute(`style`, `display: none;`);
+    let currentQuestion = 0;
+    generateQuestion();
 
-
-    let chosenAnswerEvent = document.addEventListener('click', e=> { 
+    
+    let chosenAnswerEvent = answerList.addEventListener('click', e=> { 
         e.stopPropagation();
-
-        chosenAnswer = e.target.textContent;
-        if (chosenAnswer === questionOne.answer) {
-            console.log("Correct!");
-        } else if (chosenAnswer !== questionOne.answer) {
-            console.log("Wrong!");
+        let chosenAnswer = e.target.textContent;
+        console.log(chosenAnswer);
+        console.log(questions[currentQuestion].answer)
+        
+        if (chosenAnswer !== questions[currentQuestion].answer) {
+            answerResult.textContent = "Wrong!";
+        }   else {
+            answerResult.textContent = "Correct!";
         }
-        if (questions.length === 0) {
-           console.log("Game Over!");
-        } else {
-            questions.pop();
-            chooseQuestion();
-        }
-    return chosenAnswer;
-})
+        currentQuestion++
+        generateQuestion(); 
+    });
 
-    function isCorrect() {
-        let isCorrectAnswer = (chosenAnswer === questionOne.answer);
-        return isCorrectAnswer;
-    }
+    function generateQuestion() {
+        if (currentQuestion < questions.length) {
+            let choiceArr = questions[currentQuestion].choices;
+        quizQuestions.textContent = questions[currentQuestion].question;
+            for (let i = 0; i < 4; i++) {
+            answerList.children[i].textContent = questions[currentQuestion].choices[i];
+        }
+        answerList.setAttribute(`style`, `display: block`);
+
+        }
+}
+
+}
+
+function endGame() {
     
 }
-
-
-function chooseQuestion() {
-    createListEls();
-    let randomQ = questions[Math.floor(Math.random() * questions.length)];
-    questionText = randomQ.question;
-    quizQuestions.textContent = questionText;
-    console.log(randomQ.choices);
-    for (let i = 0; i < 4; i++) {
-        answerList.children[i].textContent = randomQ.choices[i];
-    }
-}
-
-let numEls = 0;
-function createListEls() {
- 
-    if (numEls < 4) {
-        for (let i = 0; i < 4; i++) {
-        answerList.appendChild(document.createElement(`li`));
-        answerList.children[i].id = `list-item-${i + 1}`;
-        numEls++;
-    }
-}
-}
-
-
-
-
